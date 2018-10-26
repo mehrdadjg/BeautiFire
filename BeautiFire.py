00075135c3434a8e8b3a57ead3ed8ba49caba9d5
Mehrdad <mehrdadjg@gmail.com>
2018-10-26 12:01:56 -0600

    Refactor. Needs debugging.

--- BeautiFire.py
+++ BeautiFire.py
@@ -1,20 +1,34 @@
+from enum import Enum
 import re, string
 
-LEFT = 0x1
-RIGHT = 0x2
-CENTER = 0x4
-STRETCH = 0x8
+class Align(Enum):
+	LEFT = 0x0
+	RIGHT = 0x1
+	CENTER = 0x2
+	STRETCH = 0x4
+
+class SegmentFunction(Enum):
+	NORMALTEXT = 0x0
+	SINGLELINE = 0x1
+	
 
 # By putting some string in between $ signs you will force it to be shown in the same line, if possible.
 class BeautiFire:
 
 	def __init__(self):
-		self.terminal_width = 80
-		self.paragraph_second_line_indent = 4
-		self.short_line_policy = LEFT
+		## page related dimensions
+		self.page_width = 80
+		
+		## text related dimensions
+		self.par_first_line_indent = 4
+		self.par_other_lines_indent = 0
+		self.tab_width = 4
+		
+		## global paragraph properties
+		self.par_alignment = Align.LEFT
 	
-	def set_terminal_width(self, terminal_width):
-		self.terminal_width = terminal_width
+	def set_page_width(self, page_width):
+		self.page_width = page_width
 	
 	def print_paragraph(self, parahraph, indent=0):
 		if parahraph.strip() == '':
@@ -24,24 +38,27 @@ class BeautiFire:
 			raise ValueError("Paragraph contains a new line character.")
 		special_reg = re.compile(r"\$")
 		
+		## Segmentation: A segment is a maximal part of the text that has
+		##	a specific functionality
 		segments = special_reg.split(parahraph)
 		
 		index = 0
-		isSingleLine = True if parahraph[0] == '$' else False
+		segment_function = SegmentFunction.SINGLELINE if parahraph[0] == '$' else SegmentFunction.NORMALTEXT
 		
 		while index < len(segments):
 			if len(segments[index]) == 0:
-				segments[index] = [segments[index], isSingleLine]
+				segments[index] = [segments[index], segment_function]
 				index = index + 1
-				isSingleLine = not isSingleLine
+				segment_function = SegmentFunction(1 - segment_function.value)
 			elif segments[index][-1] == '\\':
 				if len(segments) > index + 1:
 					segments[index] = segments[index][:-1] + '$' + segments[index + 1]
 					del segments[index + 1]
 			else:
-				segments[index] = [segments[index], isSingleLine]
+				segments[index] = [segments[index], segment_function]
 				index = index + 1
-				isSingleLine = not isSingleLine
+				segment_function = SegmentFunction(1 - segment_function.value)
+		# End of Segmentation
 		
 		tokens = []
 		for seg_index in range(len(segments)):
@@ -72,7 +89,7 @@ class BeautiFire:
 		line_start = True
 		while index < len(tokens):
 			newToken = {False:' ', True:''}[tokens[index][1]] + tokens[index][0]
-			if len(line + newToken) <= self.terminal_width:
+			if len(line + newToken) <= self.page_width:
 				line = line + newToken
 				index = index + 1
 			else:
